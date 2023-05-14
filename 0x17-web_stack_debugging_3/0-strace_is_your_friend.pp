4a8085a9358939b65005805911ca186966b4acd2
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-05-14 16:14:34 +0100

    hot fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-include stdlib
-
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  content => file('/var/www/html/wp-settings.php'),
-}
-
-file_line { 'change_line_in_apple':
-  path  => '/var/www/html/wp-settings.php',
-  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+exec { 'fix-word-press':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
